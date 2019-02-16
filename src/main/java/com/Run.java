5e5e927323469a0307651742734ab8bc1a8949ca
unknown <tiwarivivek23@rediffmail.com>
2019-02-16 11:12:24 +0530

    commit to add v2 info

--- src/main/java/com/Run.java
+++ src/main/java/com/Run.java
@@ -3,5 +3,6 @@ package com;
 public class Run {
     public static void main(String[] args) {
         System.out.println("Run class goes here");
+        System.out.println("version 2.0, viv-examples");
     }
 }
