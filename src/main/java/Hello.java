fbd35be747e64530a570334b0f4898ea93b88344
unknown <tiwarivivek23@rediffmail.com>
2019-02-16 11:00:40 +0530

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+
+        for(String str : args){
+            System.out.println(str);
+        }
+        System.out.println("good bye");
+    }
+}
---
+++ src/main/java/com/Run.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Run class goes here");
+    }
+}
